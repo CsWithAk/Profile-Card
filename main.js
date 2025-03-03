ffa4a375db6af6dad04543c76eaf6b63f0d16ec5
Amit Kumar <amitkumarmjl22@gmail.com>
2025-03-03 19:17:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+/*document.getElementById('subscribe').addEventListener('click', function() {
+  window.open("https://youtube.com/@amit_0_prajapati?si=IFFxyuFL7UeZRmhb", "_blank");
+  this.textContent = 'Subscribed';
+});
+function message(){
+  window.open("https://www.instagram.com/direct/t/amit_0_prajapati", "_blank");
+}
+document.getElementById('message').addEventListener('click', function() {
+  window.open("https://www.instagram.com/amit_0_prajapati?igsh=MXJ1NWVqeTQ3bGN5cg==");
+  document.getElementById('subscribe').textContent = 'Subscribe';
+});*/
